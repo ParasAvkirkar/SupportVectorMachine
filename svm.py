ba65822d32fe14ec3edf2642eb20e1930e2e4144
ParasAvkirkar <parasavkirkar96@gmail.com>
2020-04-16 00:46:25 -0700

    draw implemented

--- svm.py
+++ svm.py
@@ -1,3 +1,6 @@
+# TODO: Remove use of tqdm
+from tqdm.auto import tqdm
+
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,12 +25,13 @@ def split_data_by_labels(X, y):
     positive_X = np.array(positive_X).reshape(len_p, feature_len)
     negative_X = np.array(negative_X).reshape(len_n, feature_len)
 
-    return positive_X, negative_X
+    data_dict = {-1: negative_X, 1: positive_X}
+
+    return data_dict
 
 
 def get_max_f_value(X, y):
-    positive_X, negative_X = split_data_by_labels(X, y)
-    data_dict = {-1: negative_X, 1: positive_X}
+    data_dict = split_data_by_labels(X, y)
 
     max_fval = float('-inf')
     for y_i in data_dict:
@@ -36,9 +40,12 @@ def get_max_f_value(X, y):
 
     return max_fval
 
-
+# TODO: remove this function later (using draw function instead)
 def plot(X, y):
-    positive_X, negative_X = split_data_by_labels(X, y)
+    data_dict = split_data_by_labels(X, y)
+
+    positive_X = data_dict[-1]
+    negative_X = data_dict[1]
 
     fig, ax = plt.subplots()
     ax.scatter(positive_X[:, 0], positive_X[:, 1], c='r')
@@ -53,14 +60,17 @@ def plot(X, y):
 # soft-svm with hinge-loss with gradient descent
 # because hard-svm is deterministic and we are asked to implement gradient descent
 # return weight_vector
-def train(X, y, iterations=100, lambda_param=10):
+def train(X, y):
     m = X.shape[0]
     feature_len = X.shape[1]
 
     max_fval = get_max_f_value(X, y)
     step_size = 0.1 * max_fval
 
-    T = iterations
+    # T = iterations
+    T = 3000
+    lambda_param = 1
+
     theta_t = np.zeros(feature_len)
     weight_sum = np.zeros(feature_len)
     for t in range(1, T + 1):
@@ -68,7 +78,9 @@ def train(X, y, iterations=100, lambda_param=10):
 
         weight_sum = np.add(w_t, weight_sum)
         current_weight = (1.0 / t) * weight_sum
-        print("Iteration number: " + str(t) + " Error: " + str(1 - test(X, y, current_weight)))
+
+        if t % 100 == 0:
+            print("Iteration number: " + str(t) + " Error: " + str(1 - test(X, y, current_weight)))
 
         i = np.random.choice(m)  # gets a uniformly random integer index between [0, m-1]
 
@@ -108,6 +120,29 @@ def test(X, y, w):
 def predict(w, x):
     return 1.0 if np.dot(w, x) >= 0 else -1.0
 
+def draw(X, y, hyper_plane):
+    data_dict = split_data_by_labels(X, y)
+
+    positive_X = data_dict[-1]
+    negative_X = data_dict[1]
+
+    fig, ax = plt.subplots()
+    ax.scatter(positive_X[:, 1], positive_X[:, 2], c='r')
+    ax.scatter(negative_X[:, 1], negative_X[:, 2], c='b')
+
+    ax.legend()
+    ax.grid(True)
+
+    min_v = np.amin(X) - 1
+    max_v = np.amax(X) + 1
+
+    x_axis = np.linspace(min_v, max_v, int((max_v - min_v)/0.01))
+    y_axis = (-hyper_plane[1]/hyper_plane[2]) * x_axis - hyper_plane[0]/hyper_plane[2]
+
+    ax.plot(x_axis, y_axis, '-g')
+
+
+    plt.show()
 
 if __name__ == "__main__":
     X0, y = make_blobs(n_samples=100, n_features=2, centers=2,
@@ -126,10 +161,13 @@ if __name__ == "__main__":
     train_x, train_y = X1[:split_point, :], y[:split_point, :]
     test_x, test_y = X1[split_point:, :], y[split_point:, :]
 
+    data_dict = split_data_by_labels(train_x, train_y)
     w = train(train_x, train_y)
+    print(str(w))
+    print("Norm: " + str(np.linalg.norm(w)))
     accuracy = test(test_x, test_y, w)
 
     print("Validation Accuracy: " + str(accuracy * 100) + "%")
     print("Whole accuracy: " + str(test(X1, y, w) * 100) + "%")
 
-
+    draw(X1, y, hyper_plane=w)
